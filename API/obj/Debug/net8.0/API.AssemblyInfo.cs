a750e1a47dc9dec9c13c19e04f943c22c5bd177e
19kummitha <113335352+19kummitha@users.noreply.github.com>
2025-02-06 10:15:46 +0530

    added required packages
    added required nuget packages and created data folder and inside that StoreContext class file is created for database migrations

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0694597f7ad84a319069e519d0003867debe50ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3845dad077a4f1e80019184c62ee8ec408581bcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
